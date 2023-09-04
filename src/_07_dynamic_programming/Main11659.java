0d125c6e442231a0aea8a1e402cbd2b52f5295b5
dclxxi <dclxxi@kakao.com>
2023-09-04 21:26:41 +0900

    11659. 구간 합 구하기 4 (Silver 3)

---
+++ src/_07_dynamic_programming/Main11659.java
@@ -0,0 +1,34 @@
+package _07_dynamic_programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main11659 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[] sum = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            
+            sb.append(sum[j] - sum[i - 1]).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
